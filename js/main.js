1a67fb5ce1183a5cd0edce552e5b1324a5aff8dc
Danielle Okezie <danielleokezie@yahoo.com>
2016-02-22 12:11:41 -0500

    Schedule JS

--- js/main.js
+++ js/main.js
@@ -89,6 +89,36 @@ function main() {
 
   	});
 
+if($('.schedule-box').length){
+    
+    //Tabs
+    $('.schedule-box .tab-btn').on('click', function() {
+      var target = $($(this).attr('data-id'));
+      $('.schedule-box .tab-btn').removeClass('active');
+      $(this).addClass('active');
+      $('.schedule-box .tab').fadeOut(0);
+      $('.schedule-box .tab').removeClass('current');
+      $(target).fadeIn(300);
+      $(target).addClass('current');
+      var windowWidth = $(window).width();
+      if (windowWidth <= 700) {
+        $('html, body').animate({
+           scrollTop: $('.tabs-box').offset().top
+         }, 1000);
+      }
+    });
+    
+    //Accordion
+    $('.schedule-box .hour-box .toggle-btn').on('click', function() {
+      var target = $($(this).next('.content-box'));
+      $(this).toggleClass('active');
+      $(target).slideToggle(300);
+      $(target).parents('.hour-box').toggleClass('active-box');
+    });
+    
+  }
+  
+
   	/*====================================
     Portfolio Isotope Filter
     ======================================*/
